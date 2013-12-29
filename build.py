adef6dc5cc1a066a725065a4743e536b8a473950
danasilver <dsilver1221@gmail.com>
2013-12-29 13:43:54 -0500

    add build system

---
+++ build.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import subprocess
+
+project_name = os.path.basename(os.getcwd())
+build_cmd = "jekyll build"
+s3_cmd = "s3cmd sync _site/ s3://danasilver.org"
+
+sys.stdout.write("Building Jekyll project " + project_name + ".\n")
+subprocess.Popen(build_cmd.split(" ")).communicate()
+
+sys.stdout.write("Syncing with S3.\n")
+subprocess.Popen(s3_cmd.split(" ")).communicate()
