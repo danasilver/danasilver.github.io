071357ca270abe48dc678bf68f228268cff7dcb9
danasilver <dsilver1221@gmail.com>
2013-12-30 18:00:21 -0500

    add 404 page and modify build to use awscli

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ import subprocess
 
 project_name = os.path.basename(os.getcwd())
 build_cmd = "jekyll build"
-s3_cmd = "s3cmd sync _site/ s3://danasilver.org"
+s3_cmd = "aws s3 sync _site/ s3://danasilver.org --delete"
 
 sys.stdout.write("Building Jekyll project " + project_name + ".\n")
 subprocess.Popen(build_cmd.split(" ")).communicate()
